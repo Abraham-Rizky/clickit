388637e2d390b24c22259c277b4f77073ecfae19
AJ-1988 <A.J.Jerah@hotmail.com>
2018-05-31 10:03:11 -0700

    Sticky Nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
-function toggleHam() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+let nav = document.getElementsByClassName("navigation")[0];
+let video = document.getElementById("video");
+
+/************* Burger Nav ****************/
+function myFunction(x) {
+    x.classList.toggle("change");
 }
+
+function toggleHam() {
+    nav.classList.toggle("responsive");
+}
+/************* Sticky Nav ****************/
+window.addEventListener("scroll", () => {
+  let navTop = video.offsetTop;
+  let scrollPos = window.scrollY;
+
+  if (scrollPos >= navTop + 60) {
+     nav.classList.add("navigation-fixed");
+   } else {
+     nav.classList.remove("navigation-fixed")
+   }
+})
