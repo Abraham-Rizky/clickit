d1aabf9b1924e9e6d622d52044a183e21bab4aad
Bryan Almaraz <bryan.almaraz96@gmail.com>
2018-06-07 14:05:58 -0700

    added images

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,28 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("navigation-fixed")
   }
 })
+
+
+// slider
+$(function() {
+  var width = 1200;
+  var animation = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+  var maxWidth = 3600;
+  
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slide');
+  var $slides = $slideContainer.find('.slides');
+
+  setInterval(function() {
+    $('#slider .slide').animate({'margin-left':'-='+width},animation, function(){
+      currentSlide++;
+      if(width === maxWidth) {
+        currentSlide = 1; 
+        $slideContainer.css('margin-left',0);
+      }
+    });
+  },pause)});
+
+  //setTimeout(function lastSlide(){
